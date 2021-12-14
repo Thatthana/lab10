ea454396bec10acb0fc9929c7c590abf71a13c41
Fluke <hunterx52110@gmail.com>
2021-12-14 13:11:53 +0700

    lab10_1completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,10 +2,14 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double balance ,interest ,sum ,total ,payment;	
 	cout << "Enter initial loan: ";
+	cin >> balance;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payment;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,20 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	int i=1;
+	while (balance>0){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << balance;
+	cout << setw(13) << left << (sum = (double)balance*(interest/100));
+	cout << setw(13) << left << (total = balance + sum);
+
+	if (payment > total){
+		payment = total;
+	}
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << (balance = total - payment) << endl;
+	i++;	
+	}
 	return 0;
 }
