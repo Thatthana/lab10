60946b634d7fb14bd2945ce26e38b6262845b42f
Fluke <hunterx52110@gmail.com>
2021-12-14 13:18:13 +0700

    lab10_2completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 //Write the function printO() here
+ void printO(int n,int m){
+	 if (n<=0 || m <=0){
+		 cout << "Invalid input";	 
+	 }else{
+		 for (int i=1; i<=n;i++){
+			 for(int j=1;j<=m;j++){
+				 cout << 'O';
+			 }
+			 cout << "\n";
+		 }
+	 } 
+
+ }
 
 int main(){
 	
